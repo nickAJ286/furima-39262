67bf62584f821378406aec96586a6b3f951e435e
nickAJ286 <ajiji3f@gmail.com>
2023-08-27 20:06:41 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :encrypted_password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カナを使用してください' } do
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+  validates :birthday, presence: true
 end
