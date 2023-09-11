516ee2b56940b11885ad991c3d6ca61d63d6ea92
nickAJ286 <ajiji3f@gmail.com>
2023-09-11 16:06:57 +0900

    ビューファイルとcssファイルの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :postage_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :post_day_id, presence: true
+
+  validates :image, presence: true
 end
