2018d5c7468d45ff06e8521c4be78600653e071c
nickAJ286 <ajiji3f@gmail.com>
2023-09-15 21:09:18 +0900

    発送日数のセレクトボックスの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,13 +4,14 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+  format: { with: /\A[0-9]+\z/ }
   validates :explanation, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :postage_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :post_day_id, presence: true
+  validates :post_day_id, numericality: { other_than: 1, message: "can't be blank" }
 
   validates :image, presence: true
 
@@ -19,4 +20,5 @@ class Item < ApplicationRecord
   belongs_to :status
   belongs_to :postage
   belongs_to :prefecture
+  belongs_to :post_day
 end
