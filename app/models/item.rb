dba2daec1b249acb3fd62fc090f3a79dc9e59964
nickAJ286 <ajiji3f@gmail.com>
2023-09-15 15:51:39 +0900

    商品状態のセレクトボックスの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   validates :price, presence: true
   validates :explanation, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :status_id, presence: true
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :postage_id, presence: true
   validates :prefecture_id, presence: true
   validates :post_day_id, presence: true
