38b34c2da3e68d815325fb71fd7359a13132c265
nickAJ286 <ajiji3f@gmail.com>
2023-09-15 15:44:18 +0900

    カテゴリーのセレクトボックスの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,11 +6,14 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :status_id, presence: true
   validates :postage_id, presence: true
   validates :prefecture_id, presence: true
   validates :post_day_id, presence: true
 
   validates :image, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 end
