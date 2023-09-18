c4b159822ac34d9a732b9b716de0fd694890c152
nickAJ286 <ajiji3f@gmail.com>
2023-09-18 20:55:57 +0900

    テストコードの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,17 @@
 FactoryBot.define do
   factory :item do
+    name           { Faker::Name.initials(number: 2) }
+    price          { 500 }
+    explanation    { Faker::Lorem.sentence }
+    category_id    { 2 }
+    status_id      { 2 }
+    postage_id     { 2 }
+    prefecture_id  { 2 }
+    post_day_id    { 2 }
+    association    :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
