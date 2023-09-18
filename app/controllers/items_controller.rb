45d6dd9690de95574c66ce9ccedd5c542bc5b375
nickAJ286 <ajiji3f@gmail.com>
2023-09-18 15:22:23 +0900

    出品商品の登録機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+  def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :explanation, :category_id,:status_id,:postage_id,:prefecture_id,:post_day_id,:image).merge(user_id: current_user.id)
+  end
 end
--- app/models/post_day.rb
+++ app/models/post_day.rb
@@ -1,4 +1,4 @@
-class Post_day < ActiveHash::Base
+class PostDay < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '1~2日で発送' },
