18b88c0a7e9927d5410898c729b586ca2ca0ca89
nickAJ286 <ajiji3f@gmail.com>
2023-10-02 21:45:21 +0900

    投稿枚数制限の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :post_day_id, numericality: { other_than: 1, message: "can't be blank" }
 
-  validates :images, presence: true
+  validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
