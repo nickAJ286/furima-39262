b095895dc7aada5022b403dbca0a65c5bb52f40b
nickAJ286 <ajiji3f@gmail.com>
2023-10-05 23:51:10 +0900

    商品ページ別コメント表示設定

--- app/channels/comment_channel.rb
+++ app/channels/comment_channel.rb
@@ -1,7 +1,9 @@
 class CommentChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    stream_from "comment_channel" #追加
+    # stream_from "comment_channel" #商品に紐づいたプラウザで処理する為コメントアウト
+    @item = Item.find(params[:item_id]) 
+    stream_for @item 
   end
 
   def unsubscribed
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,10 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
+    @item = Item.find(params[:item_id])
     if @comment.save
-      ActionCable.server.broadcast "comment_channel", {comment: @comment, user: @comment.user} #追加
+      CommentChannel.broadcast_to @item, { comment: @comment, user: @comment.user }
+      # ActionCable.server.broadcast "comment_channel", {comment: @comment, user: @comment.user} #商品に紐づいたプラウザで処理する為コメントアウト
       # redirect_to item_path(params[:item_id])
     end
   end
